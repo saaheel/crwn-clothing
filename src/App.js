643cb09b3267cc49dd9d50dcea185a1680bb4ac3
Saaheel Majethia <smajethia.com@gmail.com>
2022-02-24 19:31:42 +0530

    Added Collection Item & Collection Preview

--- src/App.js
+++ src/App.js
@@ -2,10 +2,8 @@ import React from 'react';
 import { Route, Switch } from "react-router-dom";
 import './App.css';
 import HomePage from "./pages/homepage/homepage.component";
+import ShopPage from './pages/shop/shop.component';
 
-const HatsPage = () => (
-  <div><h1>HatsPage</h1></div>
-)
 
 function App() {
   return (
@@ -13,7 +11,7 @@ function App() {
       {/* <HomePage /> */}
       <Switch>
       <Route exact path='/' component={HomePage} />
-      <Route path='/hats' component={HatsPage} />
+      <Route path='/shop' component={ShopPage} />
       </Switch>
     </div>
   );
