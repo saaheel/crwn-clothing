1e79be7abf8cd1d9430f633e816ed870f9f9212c
Saaheel Majethia <smajethia.com@gmail.com>
2022-03-01 12:07:48 +0530

    Fixed styles for input fields by comparing

--- src/components/custom-button/custom-button.component.jsx
+++ src/components/custom-button/custom-button.component.jsx
@@ -1,15 +1,11 @@
 import React from 'react';
 
-
 import './custom-button.styles.scss';
 
-const CustomButton = ({ children, ...otherProps }) => {
-    return ( 
-        <button className='custom-button' {...otherProps}>
-            {children}
-        </button>
-     );
+const CustomButton = ({ children, ...otherProps }) => (
+  <button className='custom-button' {...otherProps}>
+    {children}
+  </button>
+);
 
-}
- 
 export default CustomButton;
